9bc092f7edfe6b4d70fe5c1c3e98a9d87087be09
Akansha Sakhre <asakhre2002@gmail.com>
2025-08-14 10:24:59 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
       items: [
         {
           type: "doc",
-          docId: "intro",
+          docId: "Intro",
           position: "left",
           label: "Documentation",
         },
